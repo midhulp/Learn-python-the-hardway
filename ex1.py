954bd8a3e71f74327be88b9a8f9483cf83aadfa5
midhul <dhulmi3@gmail.com>
2023-05-23 16:36:09 +0530

    first python exercise in python he hard way,to print hello world

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("hello again")
+print("i like typing this")
+print("this is fun")
+print("Yay! printing")
+#print("i'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+
+
