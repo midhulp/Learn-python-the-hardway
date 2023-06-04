ed46584ab96b9f62d90dcdecf368c7c34ad6264d
midhul <dhulmi3@gmail.com>
2023-06-04 15:29:27 +0530

    Program to check uses of comment 'x'

---
+++ ex2.py
@@ -0,0 +1,10 @@
+#using comment to disable code
+
+# A comment, this is commented so you can read your program later.
+ # Anything after the '#' is ignored by python.
+
+print("I could have coded like this.") # and the comment after is ignored
+
+ # You can also use a comment to "disable" a code:
+ # print("This won't run.")
+print("This will run.")
